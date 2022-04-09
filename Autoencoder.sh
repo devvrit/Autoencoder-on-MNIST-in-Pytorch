a5a8b1dd8cfc949d1aeddbf62f990b80f636ae1c
Devvrit <devvrit.03@gmail.com>
2022-04-09 20:57:50 +0000

    .

--- Autoencoder.sh
+++ Autoencoder.sh
@@ -1,4 +1,5 @@
-python3 Autoencoder.py --quic --Lambda 0.1 --beta1 0.9 --beta2 0.999 --max_iter 4 --update 1 --warmup 5 --epochs 100 --lr 0.0078475997
-#python3 Autoencoder.py --quic --Lambda 0.1 --beta1 0.9 --beta2 0.999 --max_iter 4 --update 1 --warmup 5 --epochs 100 --lr 0.0042813324
-#python3 Autoencoder.py --quic --Lambda 0.1 --beta1 0.9 --beta2 0.999 --max_iter 4 --update 1 --warmup 5 --epochs 100 --lr 0.00233572
+#python3 Autoencoder.py --quic --Lambda 0.0 --beta1 0.9 --beta2 0.999 --max_iter 4 --update 1 --warmup 5 --epochs 100 --lr 0.0078475997
+#python3 Autoencoder.py --quic --Lambda 0.0 --beta1 0.9 --beta2 0.999 --max_iter 4 --update 1 --warmup 5 --epochs 100 --lr 0.0042813324
+#python3 Autoencoder.py --quic --Lambda 0.0 --beta1 0.9 --beta2 0.999 --max_iter 4 --update 1 --warmup 5 --epochs 100 --lr 0.00233572
+python3 Autoencoder.py --quic --Lambda 0.1 --beta1 0.9 --beta2 0.999 --max_iter 4 --update 1 --warmup 5 --epochs 100 --lr 0.0015
 # python3 Autoencoder.py --Lambda 0.5 --beta1 0.9 --beta2 1.0 --max_iter 10 --update 1 --warmup 5 --epochs 100 --lr 0.0042813324
