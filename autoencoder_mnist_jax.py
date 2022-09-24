07a12a6fa7426b40da0e44568c2b5386d0332b7b
Devvrit Khatri <devvr@google.com>
2022-09-24 00:36:10 -0500

    added bfloat16 support for tds

--- autoencoder_mnist_jax.py
+++ autoencoder_mnist_jax.py
@@ -12,12 +12,13 @@ import numpy as np                     # Ordinary NumPy
 import optax                           # Optimizers
 import tensorflow_datasets as tfds     # TFDS for MNIST
 from keras.datasets import mnist
-from typing import List
+from typing import (Any, List)
 from custom_optimizer import *
+import shampoo_jax as shampoo_jax
 
 
-flags.DEFINE_float('beta1', 0.9, help='Beta1 for Adam')
-flags.DEFINE_float('beta2', 0.999, help='Beta2 for Adam')
+flags.DEFINE_float('beta1', 0.9, help='Beta1')
+flags.DEFINE_float('beta2', 0.999, help='Beta2')
 flags.DEFINE_float('lr', 0.0001, help='Learning rate')
 flags.DEFINE_float('eps', 1e-8, help='eps')
 flags.DEFINE_integer('batch_size',
@@ -28,37 +29,79 @@ flags.DEFINE_integer('model_depth_multiplier',
                      1, help='Multiply model depth by a constant')
 flags.DEFINE_integer('warmup_epochs', 5, help='Warmup epochs')
 flags.DEFINE_integer('epochs', 10, help='#Epochs')
+flags.DEFINE_integer('t', 20, help='preconditioner computation frequency')
+flags.DEFINE_enum('dtype', 'float32', ['float32', 'bfloat16'])
+flags.DEFINE_enum('optimizer', 'shampoo', ['sgd', 'momentum', 'nesterov', 'adagrad',
+  'rmsprop', 'tds', 'shampoo', 'diag_sonew'])
 FLAGS = flags.FLAGS
 
 
 class Autoencoder(nn.Module):
   enc_hidden_states: List[int]
   dec_hidden_states: List[int]
+  dtype: Any
+  param_dtype: Any
 
   @nn.compact
   def __call__(self, x):
     for i in range(len(self.enc_hidden_states)):
       x = nn.Dense(features = self.enc_hidden_states[i],
-                   kernel_init=jnn.initializers.glorot_uniform())(x)
+                   kernel_init=jnn.initializers.glorot_uniform(),
+                   dtype=self.dtype, param_dtype=self.param_dtype)(x)
       if i<len(self.enc_hidden_states)-1:
         x = nn.tanh(x)
     for i in range(len(self.dec_hidden_states)):
       x = nn.Dense(features = self.dec_hidden_states[i],
-                   kernel_init=jnn.initializers.glorot_uniform())(x)
+                   kernel_init=jnn.initializers.glorot_uniform(),
+                   dtype=self.dtype, param_dtype=self.param_dtype)(x)
       x = nn.tanh(x)
     x = nn.Dense(features = 784,
-                 kernel_init=jnn.initializers.glorot_uniform())(x)
+                 kernel_init=jnn.initializers.glorot_uniform(),
+                 dtype=self.dtype, param_dtype=self.param_dtype)(x)
     return x
 
   def __hash__(self):
     return id(self)
 
 
+def get_optimizer(opt, learning_rate):
+  print("using optimizer:", opt)
+  if opt=="sgd":
+    return optax.sgd(learning_rate)
+  elif opt="momentum":
+    return optax.sgd(learning_rate=learning_rate, momentum=FLAGS.beta1)
+  elif opt="nesterov":
+    return optax.sgd(learning_rate=learning_rate, momentum=FLAGS.beta1, nesterov=True)
+  elif opt=="adam":
+    return optax.adam(b1=FLAGS.beta1, b2=FLAGS.beta2, eps=FLAGS.eps, learning_rate=learning_rate)
+  elif opt=="adagrad":
+    return optax.adagrad(learning_rate=learning_rate, eps=FLAGS.eps)
+  elif opt=="rmsprop":
+    return optax.rmsprop(learning_rate=learning_rate, decay=FLAGS.beta2, momentum=FLAGS.beta1, eps=FLAGS.eps)
+  elif opt=="diag_sonew":
+    return custom_optimizer.diag_sonew(learning_rate, b1=FLAGS.beta1, b2=FLAGS.beta2, eps=FLAGS.eps, adam_grafting=False)
+  elif opt=="tds":
+    return custom_optimizer.tds(learning_rate, b1=FLAGS.beta1, b2=FLAGS.beta2, eps=FLAGS.eps, transpose=True, adam_grafting=False)
+  elif opt=="shampoo":
+    return shampoo_jax.Shampoo(learning_rate = learning_rate,
+                       beta1=FLAGS.beta1,
+                       beta2=FLAGS.beta2,
+                       diagonal_epsilon=1e-10,
+                       matrix_epsilon=FLAGS.eps,
+                       weight_decay=0.0,
+                       start_preconditioning_step=25,
+                       preconditioning_compute_steps=FLAGS.t,
+                       statistics_compute_steps=1,
+                       block_size=128,
+                       best_effort_shape_interpretation=True,
+                       no_preconditioning_for_layers_with_dim_gt=8192,
+                       nesterov=False,
+                       exponent_override=0,
+                       batch_axis_name=None)
+
 def create_train_state(params, model, learning_rate):
   """Creates initial `TrainState`."""
-#   tx = optax.inject_hyperparams(optax.sgd)(learning_rate)
-  tx = optax.inject_hyperparams(tds)(learning_rate, b1=FLAGS.beta1, b2=FLAGS.beta2, eps=FLAGS.eps)
-  # tx = optax.sgd(learning_rate)
+  tx = get_optimizer(opt, learning_rate)
   return train_state.TrainState.create(
       apply_fn=model.apply, params=params, tx=tx)
 
@@ -77,10 +120,10 @@ def train_step(model, state, x):
 # @partial(jax.jit, static_argnums=0)
 def eval_step(model, state, x):
   logits = model.apply(state.params, x)
-  loss = optax.sigmoid_binary_cross_entropy(logits, x).mean(0).sum()
-  return loss
+  loss = optax.sigmoid_binary_cross_entropy(logits, x)
+  return loss.astype(jnp.float32).mean(0).sum()
 
-def train_epoch(state, model, train_ds, batch_size, epoch, rng, lrVec, train_loss_val=None):
+def train_epoch(state, model, train_ds, batch_size, epoch, rng, lrVec):
   train_ds_size = len(train_ds)
   steps_per_epoch = train_ds_size // batch_size
   print("epoch:", epoch,"and lr going to be used:", lrVec[epoch])
@@ -93,13 +136,12 @@ def train_epoch(state, model, train_ds, batch_size, epoch, rng, lrVec, train_los
   for perm in perms:
     train_x = train_ds[perm]
     state, loss = train_step(model, state, train_x)
-    batch_metrics.append(loss)
+    batch_metrics.append(loss.item())
   
   batch_metrics_np = jax.device_get(batch_metrics)
   epoch_metrics_np = np.mean(batch_metrics_np)
 
-  print('train epoch: %d, loss: %.4f' % (epoch, epoch_metrics_np))
-  #train_loss_val.create_measurement(objective_value=epoch_metrics_np, step=epoch)
+  # print('train epoch: %d, loss: %.4f' % (epoch, epoch_metrics_np))
 
   return state
 
@@ -111,14 +153,23 @@ def main(argv):
   rng, key2 = jax.random.split(rng)
   rng, key3 = jax.random.split(rng)
 
+  #Get dtype
+  if FLAGS.dtype=="floa32":
+    dtype = jnp.float32
+  elif FLAGS.dype=="bfloat16":
+    dtype = jnp.bfloat16
+
   #Generate data
   (train_inputs, _), (test_inputs, test_labels) = mnist.load_data()
-  train_inputs = train_inputs.astype(jnp.float32)
-  test_inputs = test_inputs.astype(jnp.float32)
+  train_inputs = jnp.array(train_inputs).astype(jnp.float32)
+  test_inputs = jnp.array(test_inputs).astype(jnp.float32)
 
   # Rescale input images to [0, 1]
   train_inputs = jnp.reshape(train_inputs, [-1, 784]) / 255.0
   test_inputs = jnp.reshape(test_inputs, [-1, 784]) / 255.0
+
+  train_inputs = train_inputs.astype(dtype)
+  test_inputs = test_inputs.astype(dtype)
 
   num_train_examples = train_inputs.shape[0]
   num_test_examples = test_inputs.shape[0]
@@ -136,6 +187,7 @@ def main(argv):
   encoder_decoder_sizes = encoder_sizes, decoder_sizes
 
   input_image_batch = np.random.normal(size=(batch_size,784))
+  input_image_batch = jnp.array(input_image_batch).astype(dtype)
 
   #Set learning rate schedule array
   num_epochs = FLAGS.epochs
@@ -144,15 +196,22 @@ def main(argv):
   lrVec = np.concatenate([np.linspace(0,lr,warmup_epochs),
                           np.linspace(lr,0,num_epochs-warmup_epochs+2)[1:-1]],
                          axis=0)
+  lrVec = jnp.array(lrVec).astype(dtype)
+  def autoencoder_shedule(lrVec):
+    def schedule(count):
+      bucket = count//60
+      return lrVec[bucket]
+    return schedule
 
   train_loss_val_=[]
-  model = Autoencoder(encoder_sizes, decoder_sizes)
+  model = Autoencoder(encoder_sizes, decoder_sizes, dtype=dtype, param_dtype=dtype)
   params = model.init(key3, input_image_batch)
-  state = create_train_state(params, model, FLAGS.lr)
+  state = create_train_state(params, model, autoencoder_shedule(lrVec))
+  print("Initialized model and optimizer!")
 
   for i in range(num_epochs):
     rng, key = jax.random.split(rng)
-    state = train_epoch(state, model, train_inputs, FLAGS.batch_size, i, key, lrVec, train_loss_val=None)
+    state = train_epoch(state, model, train_inputs, FLAGS.batch_size, i, key, lrVec)
     train_loss_val = eval_step(model, state, train_inputs)
     train_loss_val_.append(train_loss_val)
     print("epoch: " + str(i) +", train_loss_val: " + str(train_loss_val))
--- custom_optimizer.py
+++ custom_optimizer.py
@@ -39,28 +39,25 @@ def _update_nu(updates, nu_e, nu_d, beta2):
                            updates, nu_e)
   return nu_e, nu_d
 
-# def _scale_by_learning_rate(learning_rate: ScalarOrSchedule, flip_sign=True):
-#   m = -1 if flip_sign else 1
-#   if callable(learning_rate):
-#     return transform.scale_by_schedule(lambda count: m * learning_rate(count))
-#   return transform.scale(m * learning_rate)
 
 #Pre conditioning by tri diagonal structure
 def precondition_by_tds(
     b1: float = 0.9,
     b2: float = 0.999,
     eps: float = 1e-8,
+    transpose: bool = True,
+    adam_grafting: bool = False,
     debias: bool = True) -> optax.GradientTransformation:
 
   def init_fn(params):
     return PreconditionTriDiagonalState(
         count=jnp.zeros([], jnp.int32),
         mu = jax.tree_map(jnp.zeros_like, params),
-        nu_e=jax.tree_multimap(lambda g: jnp.zeros(len(g.reshape(-1))-1), params),
-        nu_d=jax.tree_multimap(lambda g: jnp.zeros(len(g.reshape(-1))), params))
+        nu_e=jax.tree_multimap(lambda g: jnp.zeros(len(g.reshape(-1))-1, dtype=g.dtype), params),
+        nu_d=jax.tree_multimap(lambda g: jnp.zeros(len(g.reshape(-1)), dtype=g.dtype), params))
   
   def update_fn(updates, state, params):
-    updates_hat = jax.tree_multimap(lambda g: g.reshape(-1), updates)
+    updates_hat = jax.tree_multimap(lambda g: g.T.reshape(-1) if transpose else g.reshape(-1), updates)
     mu = _update_moment(updates, state.mu, b1, 1)
     nu_e, nu_d = _update_nu(updates_hat, state.nu_e, state.nu_d, b2)
     count = state.count + jnp.array(1, dtype=jnp.int32)
@@ -69,23 +66,44 @@ def precondition_by_tds(
     nu_hat_d = nu_d if not debias else _bias_correction(nu_d, b2, count)
 
     temp = jax.tree_multimap(lambda d, e:
-                             tridiagonal_quic.tridiagKFAC(d+eps,e),
+                             tridiagonal_quic.tridiagKFAC(d,e, eps),
                              nu_hat_d, nu_hat_e)
     pre_d = jax.tree_multimap(lambda h, g: g[0], nu_hat_d, temp)
     pre_e = jax.tree_multimap(lambda h, g: g[1], nu_hat_d, temp)
-    mu_hat_flat = jax.tree_multimap(lambda m: jnp.append(jnp.append(0.0, m.reshape(-1)),0.0), mu_hat)
-    pre_e = jax.tree_multimap(lambda g: jnp.append(jnp.append(0.0, g), 0.0), pre_e)
-    updates = jax.tree_multimap(lambda mf, m, a, b:
-                                (mf[:-2]*a[:-1] + mf[1:-1]*b + mf[2:]*a[1:]).reshape(m.shape),
-                                mu_hat_flat, mu_hat, pre_e, pre_d)
+    mu_hat_flat = jax.tree_map(lambda m: m.T.reshape(-1)
+                                    if transpose else m.reshape(-1), mu_hat)
+    # Multiply gradient with diagonal
+    updates = jax.tree_map(lambda m, a: m*a, mu_hat_flat, pre_d)
+    # updates[i] = updates[i] + gradient[i-1]*pre_e[i], for i>0
+    updates = jax.tree_map(lambda u, m, a: u.at[1:].set(u[1:]+m[:-1]*a),
+                                updates, mu_hat_flat, pre_e)
+    # updates[i] = updates[i] + gradient[i+1]*pre_e[i], for i<n-1
+    updates = jax.tree_map(lambda u, m, a: u.at[:-1].set(u[:-1]+m[1:]*a),
+                                updates, mu_hat_flat, pre_e)
+    # reshape them to the original param shapes
+    updates = jax.tree_map(lambda mf, m: mf.reshape(m.T.shape).T
+                                if transpose else mf.reshape(m.shape),
+                                updates, mu_hat)
+    # if adam_grafting:
+    #   adam_updates = jax.tree_map(lambda m, v: m / (jnp.sqrt(v) + eps),
+    #                                  mu_hat, nu_hat)
+    #   updates = jax.tree_map(
+    #       lambda u, au: (jnp.linalg.norm(au)/(jnp.linalg.norm(u))+1e-12) * u,
+    #       updates, adam_updates)
+
+    # mu_hat_flat = jax.tree_multimap(lambda m: jnp.append(jnp.append(0.0, m.reshape(-1)),0.0), mu_hat)
+    # pre_e = jax.tree_multimap(lambda g: jnp.append(jnp.append(0.0, g), 0.0), pre_e)
+    # updates = jax.tree_multimap(lambda mf, m, a, b:
+    #                             (mf[:-2]*a[:-1] + mf[1:-1]*b + mf[2:]*a[1:]).reshape(m.shape),
+    #                             mu_hat_flat, mu_hat, pre_e, pre_d)
     return updates, PreconditionTriDiagonalState(count=count, mu=mu, nu_e=nu_e,
                                                  nu_d=nu_d)
 
   return optax.GradientTransformation(init_fn, update_fn)
 
-def tds(learning_rate: ScalarOrSchedule, b1=0.9, b2=0.99, eps=1e-8):
+def tds(learning_rate: ScalarOrSchedule, b1=0.9, b2=0.99, eps=1e-8, transpose=True, adam_grafting=False):
     return combine.chain(
       precondition_by_tds(
-          b1=b1, b2=b2, eps=eps),
+          b1=b1, b2=b2, eps=eps, transpose=transpose, adam_grafting=adam_grafting),
         alias._scale_by_learning_rate(learning_rate),
     )
