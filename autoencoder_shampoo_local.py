27cef7b4ba4977d2b872d9dcdee0de86ba4cdf00
Devvrit Khatri <devvr@google.com>
2022-09-24 23:48:10 -0500

    bug fixes

--- autoencoder_shampoo_local.py
+++ autoencoder_shampoo_local.py
@@ -2914,20 +2914,25 @@ parser.add_argument('--dtype', type=str, default="float32", help="float32 or bfl
 
 FLAGS = parser.parse_args(args=[])
 
-mode=0
+mode=1
 if mode==1:
   FLAGS.beta1 = 9.00055e-1
   FLAGS.beta2 = 9.50822e-1
   FLAGS.lr = 3.70288e-3
   FLAGS.eps = 9.66177e-9
   FLAGS.t = 20
-if model==2:
+if mode==2:
   FLAGS.beta1 = 9.0e-1
   FLAGS.beta2 = 9.4925e-1
   FLAGS.lr = 3.5338e-3
   FLAGS.eps = 1e-6
   FLAGS.t = 1
-
+if mode==3:
+  FLAGS.beta1 = 0.9
+  FLAGS.beta2 = 9.43936e-1
+  FLAGS.lr = 3.51539e-3
+  FLAGS.eps = 1e-6
+  FLAGS.t = 10
 
 
 
